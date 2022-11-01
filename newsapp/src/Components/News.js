0140fee4c86d831a2d61fa506362a49e25716547
pandit <dhamdherepandit@gmail.com>
2022-11-01 11:20:04 +0530

    news img added

--- newsapp/src/Components/News.js
+++ newsapp/src/Components/News.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 
 export class News extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Newsitem title="myTitle" description="mydesc"/>
-        <Newsitem/>
-        <Newsitem/>
-        <Newsitem/>
-        <Newsitem/>
-        <Newsitem/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="container my-3">
+                <h2>NewsMonkey-Top Headlines</h2>
+                <div className="row">
+                    <div className="col-md-4">
+                <Newsitem title="myTitle" description="mydesc" />
+                </div>
+                <div className="col-md-4">
+                <Newsitem title="myTitle" description="mydesc" />
+                </div>
+                <div className="col-md-4">
+                <Newsitem title="myTitle" description="mydesc" />
+                </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default News
