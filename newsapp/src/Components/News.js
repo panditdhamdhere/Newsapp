d8cef669d12f0b4eed89f7ae68058fe62a5e1010
pandit <dhamdherepandit@gmail.com>
2022-11-02 16:46:18 +0530

    news added real

--- newsapp/src/Components/News.js
+++ newsapp/src/Components/News.js
@@ -43,15 +43,13 @@ export class News extends Component {
             <div className="container my-3">
                 <h2>NewsMonkey-Top Headlines</h2>
                 <div className="row">
-                    <div className="col-md-4">
-                        <Newsitem title="myTitle" description="mydesc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" newsUrl="TODO"/>
-                    </div>
-                    <div className="col-md-4">
-                        <Newsitem title="myTitle" description="mydesc" />
-                    </div>
-                    <div className="col-md-4">
-                        <Newsitem title="myTitle" description="mydesc" />
-                    </div>
+                    {this.state.articles.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
+                            <Newsitem  title={element.title.slice(0,44)} description={element.description.slice(0,88)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                        </div>
+
+                    })}
+
                 </div>
             </div>
         )
--- newsapp/src/Components/Newsitem.js
+++ newsapp/src/Components/Newsitem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
     render() {
-        let { title, description, imageUrl, } = this.props;
+        let { title, description, imageUrl, newsUrl } = this.props;
         return (
             <div className="my-3">
                 <div className="card" style={{ width: "18rem;" }}>
                     <img src={imageUrl} className="card-img-top" alt="news pic" />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href="/Newsdetail" className="btn btn-sm btn-primary">Read More</a>
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
