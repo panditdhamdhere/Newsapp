b0b8399e0e94fbfdeb12a8e713fd301d3d4a4b00
pandit <dhamdherepandit@gmail.com>
2022-11-03 17:37:41 +0530

    spinner gif added

--- newsapp/src/Components/News.js
+++ newsapp/src/Components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Newsitem from './Newsitem'
+import Spinner from './Spinner';
 
 export class News extends Component {
     articles = [
@@ -13,7 +14,7 @@ export class News extends Component {
             "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
             "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
             "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
+            "publishedAt": "55-04-27T11:41:47Z",
             "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
         },
         {
@@ -26,7 +27,7 @@ export class News extends Component {
             "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
             "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
             "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
+            "publishedAt": "55-03-30T15:26:05Z",
             "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
         }
     ]
@@ -41,7 +42,7 @@ export class News extends Component {
     }
 
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=6c918298bcfa4cf38a64b35e98f5a148&page=1&pageSize=20";
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6c918298bcfa4cf38a64b35e98f5a148&page=1&pageSize=5${this.props.pageSize}`;
         let data = await fetch(url);
         let parseData = await data.json()
         console.log(parseData);
@@ -51,7 +52,7 @@ export class News extends Component {
     handlePrevClick = async () => {
         console.log("Previous");
 
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6c918298bcfa4cf38a64b35e98f5a148&page=${this.state.page - 1}&pageSize=20`;
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6c918298bcfa4cf38a64b35e98f5a148&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parseData = await data.json()
         console.log(parseData);
@@ -64,11 +65,11 @@ export class News extends Component {
 
     handleNextClick = async () => {
         console.log("Next");
-        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 5)) {
 
         }
         else {
-            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6c918298bcfa4cf38a64b35e98f5a148&page=${this.state.page + 1}&pageSize=20`;
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6c918298bcfa4cf38a64b35e98f5a148&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
             let data = await fetch(url);
             let parseData = await data.json()
             console.log(parseData);
@@ -84,7 +85,8 @@ export class News extends Component {
         console.log("render");
         return (
             <div className="container my-3">
-                <h2>NewsMonkey-Top Headlines</h2>
+                <h1 className="text-center">NewsMonkey-Top Headlines</h1>
+                <Spinner />
                 <div className="row">
                     {this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
@@ -95,7 +97,7 @@ export class News extends Component {
                 </div>
                 <div className="container d-flex justify-content-between">
                     <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
-                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 5)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
